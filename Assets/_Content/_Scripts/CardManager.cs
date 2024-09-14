eb80f34656a8d54d0d7d14d04460d5ef9bb14936
Peter Rady <33478871+PetroGXR@users.noreply.github.com>
2024-09-15 02:58:02 +0300

    - initial card prefab

---
+++ Assets/_Content/_Scripts/CardManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PetroGXR.WhisperingDoubles
+{
+    public class CardManager : MonoBehaviour
+    {
+        [SerializeField] Image imageFace;
+        [SerializeField] Image imageBack;
+    }
+}
