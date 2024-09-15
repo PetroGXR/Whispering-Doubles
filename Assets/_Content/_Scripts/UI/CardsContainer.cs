32f031d3c9afcf04e5b4a844394b2d63d792ab7d
Peter Rady <33478871+PetroGXR@users.noreply.github.com>
2024-09-15 17:25:24 +0300

    - add propert to get max cards the conatiner can have with the min size of card to limit the cards generation for levels

--- Assets/_Content/_Scripts/UI/CardsContainer.cs
+++ Assets/_Content/_Scripts/UI/CardsContainer.cs
@@ -7,11 +7,34 @@ namespace PetroGXR.WhisperingDoubles.UI
 {
     public class CardsContainer : MonoBehaviour
     {
+        [Header("Settings")]
+        [SerializeField][Range(0.5f, 5f)] float showCardsDuration = 1f;
+        [SerializeField][Range(128f, 512f)] float minCardSize = 256f;
+
+        [Header("UI")]
         [SerializeField] GridLayoutGroup layoutGroup;
         [SerializeField] GameObject prefabCard;
-        [SerializeField] [Range(0.5f, 5f)] float showCardsDuration = 1f;
+
+        public int MaxCards
+        {
+            private set;
+            get;
+        }
 
         private List<Card> cards = new List<Card>();
+
+        private void Start()
+        {
+            RectTransform rectCards = layoutGroup.transform as RectTransform;
+            int rows = Mathf.CeilToInt(rectCards.rect.height / (minCardSize + layoutGroup.spacing.y));
+            int columns = Mathf.CeilToInt(rectCards.rect.width / (minCardSize + layoutGroup.spacing.x));
+            MaxCards = rows * columns;
+            
+            if(MaxCards % 2 != 0 )
+            {
+                MaxCards--;
+            }
+        }
 
         public void Setup(string back, List<string> faces)
         {
@@ -19,13 +42,12 @@ namespace PetroGXR.WhisperingDoubles.UI
 
             float ratio = rectCards.rect.height / rectCards.rect.width;
             int columns = 1;
-            int rows = Mathf.FloorToInt(columns * ratio);
+            int rows = Mathf.CeilToInt(columns * ratio);
 
             while (rows * columns < faces.Count)
             {
                 columns++;
-                rows = Mathf.FloorToInt(columns * ratio);
-                
+                rows = Mathf.CeilToInt(columns * ratio);
             }
 
             float minSize = Mathf.Min((rectCards.rect.width - columns * layoutGroup.spacing.x) / columns, (rectCards.rect.height - rows * layoutGroup.spacing.y) / rows);
